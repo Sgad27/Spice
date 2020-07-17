0ef5eb81468fb504a1283d105b6c4662c4b764d8
samsher2196 <samshergadkary2196@gmail.com>
2020-07-17 16:43:16 +0530

    Removing unnecessary references

--- Spice/Data/DbInitializer.cs
+++ Spice/Data/DbInitializer.cs
@@ -3,9 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Spice.Models;
 using Spice.Utility;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Spice.Data
 {
